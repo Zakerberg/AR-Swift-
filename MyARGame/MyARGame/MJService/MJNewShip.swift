54b504116d0778ba0127de7761b2e6c48de809ae
Zakerberg <blmichael1129@icloud.com>
2017-07-30 09:43:34 +0800

    飞船随机位置

--- MyARGame/MyARGame/MJService/MJNewShip.swift
+++ MyARGame/MyARGame/MJService/MJNewShip.swift
@@ -9,19 +9,30 @@
 import SceneKit
 import ARKit
 
-
 extension ARSCNView {
     
     func newShip() {
         
         let newShip = MJShipModel ()
         
-        let x = 0.5
-        let y = 0.5
-        let z = -1.0
-        
-        newShip.position = SCNVector3(x, y, z)
-        self.scene.rootNode.addChildNode(newShip)
+        let x = -0.5[0.5]
+        let y = -0.5[0.5]
 
+        newShip.position = SCNVector3(x, y, -1)
+        self.scene.rootNode.addChildNode(newShip)
+    }
+}
+
+extension Double {
+    
+    subscript(_ end : Double) -> Float {
+        
+        // 随机0 - 1 之间的小数
+        let factor =   Double(arc4random()) / Double(UInt32.max)
+        
+        // 1 - 100之间的随机数  算法: 用大数 减去区间内的一个数
+        // 100 - (100 - 1) *factor
+        
+        return Float(end - factor * (end - self))
     }
 }
